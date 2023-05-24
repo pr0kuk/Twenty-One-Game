c950474cf5b66d3766691edd76e713292b58561f
pr0kuk <alex-shch@yandex.ru>
2023-05-24 17:43:08 +0300

    added max framerate
    deleted extra variables
    replaced click with press

--- Game.java
+++ Game.java
@@ -19,13 +19,98 @@ public class Game extends Canvas implements Runnable, MouseListener {
    static int[] num_cards = new int[2];
    static int[] card_sum = new int[2];
    static int balance = 100, bank = 100, stake = 0;
-   static int ready = 0, card = 0;
    static Sprite shirt, back, box;
    static Sprite[][] spritescard = new Sprite[12][4];
 
    public void start() {
       running = true;
       new Thread(this).start();
+   }
+
+
+   public void stake(int sum) {
+      balance -= sum;
+      stake = sum;
+   }
+   public void get_card(int player) {
+      int a, b;
+      do {
+         a = (int)(Math.random() * 10) + 2;
+         b = (int)(Math.random() * 4);
+      } while (cards[a][b] == 1 || a==5);
+      cards[a][b] = 1;
+      card_sum[player] += a;
+      hand[player][num_cards[player]][0] = a;
+      hand[player][num_cards[player]][1] = b;
+      num_cards[player]++;
+   }
+
+   public void check() {
+      if (card_sum[1] > 21)
+         lose();
+      else if (card_sum[1] == 21)
+         win();
+   }
+
+   public void play_bot() {
+      do {
+         get_card(0);
+      } while(card_sum[0] < 17);
+      if (Math.abs(card_sum[0]-21) <= Math.abs(card_sum[1]-21))
+         lose();
+      else
+         win();
+   }
+
+   public void lose() {
+      bank += stake;
+      stake = 0;
+      lose = true;
+   }
+   public void win() {
+      bank -= stake;
+      balance += (2 * stake);
+      stake = 0;
+      win = true;  
+   }
+
+   public void loadsprites()
+   {
+      back = new Sprite("textures\\table.png").width(WIDTH).height(HEIGHT);
+      box = new Sprite("textures\\d00237box.jpg").width(CARDW).height(CARDH);
+      shirt = new Sprite("textures\\shirt.jpg").width(CARDW).height(CARDH);
+      String ss, s1 = "", s2= "";
+      for (int i = 2; i <= 11; i++)
+      {
+         if (i == 5) i++;
+         switch(i) {
+            case (2):
+               s1 = "U"; break;
+            case(3):
+               s1 = "O"; break;
+            case(4):
+               s1 = "K"; break;
+            case(11):
+               s1 = "A"; break;
+            default:
+               s1 = ""+i;
+         }
+         for (int j = 0; j < 4; j++)
+         {
+            switch(j) {
+               case(0):
+                  s2 = "c"; break;
+               case(1):
+                  s2 = "d"; break;
+               case(2):
+                  s2 = "h"; break;
+               case(3):
+                  s2 = "s"; break;
+            }
+            ss = s2 + s1;
+            spritescard[i][j] = new Sprite("textures\\d00237d03" + ss + ".jpg").width(CARDW).height(CARDH);
+         }
+      }
    }
 
    public void drawhand(Graphics g, int player) {
@@ -58,9 +143,9 @@ public class Game extends Canvas implements Runnable, MouseListener {
       int sh = (int)g.getFontMetrics().getStringBounds(s, g).getHeight();
       int xc = x - sw / 2;
       int yc = y - sh / 2;
-      //System.out.println(xc+" "+yc+" "+sw+" "+sh);
       g.drawString(s, xc, y);
    }
+
    public void  render() {
       BufferStrategy bs = getBufferStrategy(); 
       if (bs == null) {
@@ -75,7 +160,6 @@ public class Game extends Canvas implements Runnable, MouseListener {
 
       box.center_draw(g,950,400);
       shirt.center_draw(g,1050,400);
-
 
       drawhand(g, 0);
       drawhand(g, 1);
@@ -114,143 +198,40 @@ public class Game extends Canvas implements Runnable, MouseListener {
       bs.show();
    }
 
-   public Boolean ClickInRect(int X, int Y, int x1, int y1, int x2, int y2) {
-      if (X > x1 && X < (x2+x1) && Y > y1 && Y < (y2+y1))
-         return true;
-      return false;
-   }
-   public void stake(int sum) {
-      balance -= sum;
-      stake = sum;
-   }
-   public void get_card(int player) {
-      int a, b;
-      do {
-         a = (int)(Math.random() * 10) + 2;
-         b = (int)(Math.random() * 4);
-      } while (cards[a][b] == 1 || a==5);
-      cards[a][b] = 1;
-      card_sum[player] += a;
-      hand[player][num_cards[player]][0] = a;
-      hand[player][num_cards[player]][1] = b;
-      num_cards[player]++;
-   }
-
-   public void check() {
-      if (card_sum[1] > 21)
-         lose();
-      else if (card_sum[1] == 21)
-         win();
-      else if (ready == 1)
-            play_bot();
-   }
-
-   public void play_bot() {
-      do {
-         get_card(0);
-      } while(card_sum[0] < 17);
-      if (Math.abs(card_sum[0]-21) <= Math.abs(card_sum[1]-21))
-         lose();
-      else
-         win();
-      ready = 0;
-   }
-
    public void update(int X, int Y) {
       if (lose == false && win == false && startflag == false) {
          if (ClickInRect(X,Y, 1800, 800, 100, 50) == true) {
             get_card(1);
             check();
          }
-         if (ClickInRect(X,Y, 1800, 875, 100, 50) == true) {
+         if (ClickInRect(X,Y, 1800, 875, 100, 50) == true)
             play_bot();
-         }
       }
       else {
-         if (ClickInRect(X,Y, LEFTSTAKEV, STAKEH, STAKESIZE, STAKESIZE) == true)
+         if (ClickInRect(X, Y, LEFTSTAKEV, STAKEH, STAKESIZE, STAKESIZE) == true)
             stake(5);
-         else if (ClickInRect(X,Y, LEFTSTAKEV +(int)(1.2*STAKESIZE), STAKEH, STAKESIZE, STAKESIZE) == true)
+         else if (ClickInRect(X, Y, LEFTSTAKEV + (int)(1.2*1*STAKESIZE), STAKEH, STAKESIZE, STAKESIZE) == true)
             stake(10);
-         else if (ClickInRect(X,Y, LEFTSTAKEV +(int)(1.2*2*STAKESIZE), STAKEH, STAKESIZE, STAKESIZE) == true)
+         else if (ClickInRect(X, Y, LEFTSTAKEV + (int)(1.2*2*STAKESIZE), STAKEH, STAKESIZE, STAKESIZE) == true)
             stake(15);
-         else if (ClickInRect(X,Y, LEFTSTAKEV +(int)(1.2*3*STAKESIZE), STAKEH, STAKESIZE, STAKESIZE) == true)
+         else if (ClickInRect(X, Y, LEFTSTAKEV + (int)(1.2*3*STAKESIZE), STAKEH, STAKESIZE, STAKESIZE) == true)
             stake(20);
          else
             return;
-         card_sum[0] = 0;
-         card_sum[1] = 0;
-         num_cards[0] = 0;
-         num_cards[1] = 0;
-         lose = false;
-         win = false;
-         startflag = false;
+         card_sum[0] = card_sum[1] = num_cards[0] = num_cards[1] = 0;
+         lose = win = startflag = false;
       }
    }
 
-   public void lose() {
-      bank += stake;
-      stake = 0;
-      lose = true;
-   }
-   public void win() {
-      bank -= stake;
-      balance += (2 * stake);
-      stake = 0;
-      win = true;  
-   }
-   public void loadsprites()
-   {
-      back = new Sprite("textures\\table.png").width(WIDTH).height(HEIGHT);
-      box = new Sprite("textures\\d00237box.jpg").width(CARDW).height(CARDH);
-      shirt = new Sprite("textures\\shirt.jpg").width(CARDW).height(CARDH);
-      String ss, s1 ="", s2="";
-      for (int i = 2; i <= 11; i++)
-      {
-         if (i == 5) i++;
-         switch(i) {
-            case (2):
-               s1 = "U";
-               break;
-            case(3):
-               s1 = "O";
-               break;
-            case(4):
-               s1 = "K";
-               break;
-            case(11):
-               s1 = "A";
-               break;
-            default:
-               s1 = ""+i;
-         }
-         for (int j = 0; j < 4; j++)
-         {
-            switch(j) {
-               case(0):
-                  s2 = "c";
-                  break;
-               case(1):
-                  s2 = "d";
-                  break;
-               case(2):
-                  s2 = "h";
-                  break;
-               case(3):
-                  s2 = "s";
-                  break;
-            }
-            ss = s2 + s1;
-            //System.out.println("textures\\d00237d03" + ss + ".jpg");
-            spritescard[i][j] = new Sprite("textures\\d00237d03" + ss + ".jpg").width(CARDW).height(CARDH);
-         }
-      }
-   }
    public void run() {
       addMouseListener(this);
       loadsprites();
-      
-      while(running) {
-         render();
+      long delta = 0, lastTime = 0;
+      while(running){
+         delta = System.currentTimeMillis() - lastTime;
+         if (delta >= 500)
+			   lastTime = System.currentTimeMillis();	
+            render();
       }
    }
 
@@ -267,9 +248,15 @@ public class Game extends Canvas implements Runnable, MouseListener {
       game.start();
    }
 
-   public void mouseClicked(MouseEvent e) {update(e.getX(), e.getY());}
+
+   public Boolean ClickInRect(int X, int Y, int x1, int y1, int x2, int y2) {
+      if (X > x1 && X < (x2+x1) && Y > y1 && Y < (y2+y1))
+         return true;
+      return false;
+   }
+   public void mouseClicked(MouseEvent e) {}
    public void mouseEntered(MouseEvent e) {}
    public void mouseExited(MouseEvent e) {}
-   public void mousePressed(MouseEvent e) {}
+   public void mousePressed(MouseEvent e) {update(e.getX(), e.getY());}
    public void mouseReleased(MouseEvent e) {}
 }
